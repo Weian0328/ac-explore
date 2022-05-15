df37699c67eebe7a56a9cf0fdcfbfe7900ef5c12
weian <kevin19960328@gmail.com>
2022-05-15 14:52:17 +0800

    first commit

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,23 @@
+import consumer from "./consumer";
+
+document.addEventListener("turbolinks:load", () => {
+  const element = document.getElementById("room-id");
+  const room_id = element.getAttribute("data-room-id");
+
+  consumer.subscriptions.create(
+    { channel: "RoomChannel", room_id: room_id },
+    {
+      connected() {
+        console.log("Connected room channel" + room_id);
+      },
+
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+      },
+
+      received(data) {
+        console.log(data);
+      },
+    }
+  );
+});
